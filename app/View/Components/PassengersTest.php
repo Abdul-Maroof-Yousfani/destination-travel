d707694202a6c0bd0753b60186825ef1c5dbb76a
Syed Ali <124593789+SyedAliiii@users.noreply.github.com>
2025-05-13 18:05:09 +0500

    Update and rename passengerstest.php to PassengersTest.php

--- app/View/Components/PassengersTest.php
+++ app/View/Components/PassengersTest.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class passengerstest extends Component
+class PassengersTest extends Component
 {
     public $flightData;
     /**
@@ -22,6 +22,6 @@ class passengerstest extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.passengerstest');
+        return view('components.PassengersTest');
     }
 }
