062034c989e089a10ab391b2ecc084a4f02fd496
Syed Ali <124593789+SyedAliiii@users.noreply.github.com>
2025-05-13 17:43:24 +0500

    Update and rename passengers.php to Passengers.php

--- app/View/Components/Passengers.php
+++ app/View/Components/Passengers.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class passengers extends Component
+class Passengers extends Component
 {
     public $flightData;
     /**
